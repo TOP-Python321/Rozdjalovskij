893390bbb0b1cd33d59bd68038c281791ff29f4b
ra939152 <129759184+ra939152@users.noreply.github.com>
2023-04-20 15:06:03 +0500

    В папку #HW 2023.04.09 добавлены выполненные задания

---
+++ 2023.04.09/3.py
@@ -0,0 +1,8 @@
+time = int(input())
+h = time // 60
+m = time % 60
+print(f"{time} минут - это {h} час {m} мин")
+
+# C:\Python_HW>py 1.3.py
+# 150
+# 150 минут - это 2 час 30 мин
---
+++ 2023.04.09/5.py
@@ -0,0 +1,13 @@
+m1 = int(input())
+m2 = int(input())
+m3 = m2 /10
+m4 = m1 + m3
+km1 = m4 * 1.61
+km2 = round(km1, 1)
+print(f"{m4} миль = {km2} км")
+
+# C:\Python_HW>py 1.5.py
+# 15
+# 7
+# 0.7
+# 15.7 миль = 25.3 км
