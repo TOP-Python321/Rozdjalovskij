e584ff166be86428a5f4d6ec36ad13d7d57a747d
ra939152 <129759184+ra939152@users.noreply.github.com>
2023-04-27 11:45:47 +0500

    add HW 2023.04.16

---
+++ 2023.04.16/03.py
@@ -0,0 +1,8 @@
+year = int(input())
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print(f"\nда")
+ 
+# C:\Users\user\Desktop\Учеба\Python\Rozdjalovskij\2023.04.16>py 3.py
+# 2020
+
+# да
